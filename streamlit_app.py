b2c473ca707b05edde70aaa12a698759bad52998
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2024-01-05 13:41:34 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,10 @@ def run_query(query):
         return cur.fetchall()
     
 #Queries
-rows = run_query("select lead_source, lead_created_date, sum(total_leads), sum(convertedleads), sum(verifiedleads) from CD_ANALYTICS_TESTDB.OMKAR.SPRING_ADS_DASHBOARD where lead_Created_date is not null  and lead_source in ('FACEBOOK', 'GOOGLE', 'FACEBOOKSPRING', 'GOOGLEPMAX', 'TIKTOK', 'GOOGLE BRANDED') order by 2 group by 1,2;")
+rows = run_query('''select lead_source, lead_created_date, sum(total_leads), sum(convertedleads), sum(verifiedleads) 
+                  from CD_ANALYTICS_TESTDB.OMKAR.SPRING_ADS_DASHBOARD where lead_Created_date is not null and lead_source in 
+                  ('FACEBOOK', 'GOOGLE', 'FACEBOOKSPRING', 'GOOGLEPMAX', 'TIKTOK', 'GOOGLE BRANDED') order by 2 
+                   group by 1,2;''')
                   
 df=pd.DataFrame(rows)
 df.columns += 1
